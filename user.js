7749e38f3374d0cd3496bc6706123df028575c81
Malik <malikmukhtar63@gmail.com>
2020-04-23 06:24:53 +0100

    modified user.js

--- user.js
+++ user.js
@@ -1,14 +1,5 @@
-// window.onload(myFunctionCart());
-// function myFunctionCart() {
-//   document.getElementById("cart").innerHTML = Math.ceil(Math.random() * 10);
-//   alert("Welcome to Photosnap Gadget Inc");
-// }
-
-const view = document.getElementById("view-button");
-
-view.addEventListener("click", displayItem);
-
-function displayItem() {
-  console.log("am here");
-  document.getElementById("");
+window.onload(myFunctionCart());
+function myFunctionCart() {
+  document.getElementById("cart").innerHTML = Math.ceil(Math.random() * 10);
+  alert("Welcome to Photosnap Gadget Inc");
 }
